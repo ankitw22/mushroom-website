feea8a8e4f0939c22a47ce0ac27f3c3c1b13b9a8
ankitw22 <ankit@whozzat.com>
2026-04-11 13:20:26 +0530

    feat: add Google Analytics tracking tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -136,6 +136,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8YV0EJ00KZ"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-8YV0EJ00KZ');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
